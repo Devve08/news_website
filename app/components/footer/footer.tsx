830ee5f11fa5f0c056065c49b076681a28721da7
Devve08 <mhamad.safa.08@example.com>
2023-09-11 01:16:51 -0500

    footer

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Footer = () => {
+  return (
+    <div className=' bg-primary min-h-screen  w-full overflow-hidden relative'>
+      <div className='curve'></div>
+    </div>
+  );
+};
+
+export default Footer;
